b8b130a74c48d80c34113c21303379c1570031ae
HAIER\20011709 <18330011253@163.com>
2021-02-01 10:56:56 +0800

    测试tj代码

--- src/main/java/com/xbd/pushdata/test/a.java
+++ src/main/java/com/xbd/pushdata/test/a.java
@@ -4,5 +4,6 @@ public class a {
     public static void main(String[] args) {
         System.out.println("test1111");
         System.out.println("test1111");
+        System.out.println("test1112");
     }
 }
