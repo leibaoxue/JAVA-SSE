5e71acde5dce63c7734f6be49079bec9e755e3ad
HAIER\20011709 <18330011253@163.com>
2021-02-01 10:31:45 +0800

    测试tj代码

--- src/main/java/com/xbd/pushdata/test/a.java
+++ src/main/java/com/xbd/pushdata/test/a.java
@@ -2,6 +2,6 @@ package com.xbd.pushdata.test;
 
 public class a {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test1111");
     }
 }
