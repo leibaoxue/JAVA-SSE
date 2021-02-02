5a8649e6b6cb91fab5eb27d6cd62986d8ea515dd
HAIER\20011709 <18330011253@163.com>
2021-02-02 14:56:04 +0800

    测试test更改代码

--- src/main/java/com/xbd/pushdata/test/Test.java
+++ src/main/java/com/xbd/pushdata/test/Test.java
@@ -17,7 +17,7 @@ public class Test {
 	}
 
 	public static void main(String[] args) {
-		List<String> al = Arrays.asList("a", "b", "c", "d");
+		List<String> al = Arrays.asList("a", "b", "c", "d","e");
 		//(1)方法一：
 		al.forEach(Test::printValur);
 		//(2)方法二：
