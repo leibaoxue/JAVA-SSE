c878136ba590e14b78fea304c957d70e473cd99b
HAIER\20011709 <18330011253@163.com>
2021-02-02 14:52:06 +0800

    测试test代码

---
+++ src/main/java/com/xbd/pushdata/test/Test.java
@@ -0,0 +1,33 @@
+package com.xbd.pushdata.test;
+
+import java.sql.Wrapper;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.LambdaUtil;
+
+public class Test {
+	public static void printValur(String str) {
+		System.out.println("print value : " + str);
+	}
+
+	public static void main(String[] args) {
+		List<String> al = Arrays.asList("a", "b", "c", "d");
+		//(1)方法一：
+		al.forEach(Test::printValur);
+		//(2)方法二：
+		// 下面的方法和上面等价的
+		Consumer<String> methodParam = Test::printValur; // 方法参数
+		al.forEach(x -> methodParam.accept(x));// 方法执行accept
+	}
+}
+@Data
+class Student {
+	private String name;
+	private String sex;
+}
